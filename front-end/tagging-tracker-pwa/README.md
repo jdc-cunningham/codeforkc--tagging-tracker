2c5980f4c0d947a3655a12eb4838f82089b3844f
Jacob Cunningham <jcun4128@gmail.com>
2020-01-29 20:45:21 -0600

    functional storage and loading of images from indexed db via dexie

--- front-end/tagging-tracker-pwa/README.md
+++ front-end/tagging-tracker-pwa/README.md
@@ -1,68 +1,28 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### About
+#### local file storage
+This app uses [dexie.js IndexedDB wrapper](https://github.com/dfahlander/Dexie.js/) to store the images as strings for offline use. Then when back online again, the photos are uploaded to a Node API that then goes to AWS S3. The backend has image resizing so for display of thumbnails those are tiny files.
 
-## Available Scripts
+**note** that has an ISC license, something about telling what it's using, guess add an about/info somewhere.
 
-In the project directory, you can run:
+The size of files locally(on device) are tracked so if there is a space issue, can let user know to upload or delete some images. This issue should be avoided by using Chrome to install the PWA(Add to home)
 
-### `npm start`
+**note** Chrome has bigger local file storage(IndexedDB) vs. Safari
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### dependencies
+* node
+* npm
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+### install
+* clone this repo
+* run npm install which should install all the dependencies needed
 
-### `npm test`
+### Stack
+The front end is built with `create-react-app`, it's all ReactJS with SCSS. All functional components and hooks using plain state/props. `dexie.js` is just for storing the images unless they're uploaded right away.
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+### Auth
+Intending to use JWT with the Node backend. No free sign up yet, will be added though with Mailgun for emails and all that. Some demo accounts will be generated initially.
 
 ### Deployment
+run `npm run build` to generate static files then upload to a static host
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Built with create-react-app
--- front-end/tagging-tracker-pwa/src/App.js
+++ front-end/tagging-tracker-pwa/src/App.js
@@ -1,23 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Dexie from 'dexie';
 
 const App = () => {
 	const [camera, setCamera] = useState({ enabled: false }); // not sure if needed
-	const [photo, setPhoto] = useState({ src: "" });
+	const [loadedPhoto, setLoadedPhoto] = useState({ src: "", meta: {} });
+	const [savedPhotos, setSavedPhotos] = useState([]);
+	const [offlineStorage, setOfflineStorage] = useState(null);
+	// const 
 
 	const fileInput = useRef(null);
 	const photoPreview = useRef(null);
-
-	const getMedia = async (constraints) => {
-		let stream = null;
-		try {
-		  stream = await navigator.mediaDevices.getUserMedia(constraints);
-		  /* use the stream */
-		} catch(err) {
-			alert('Failed to get camera');
-		}
-	  }
 
 	const openCamera = () => {
 		fileInput.current.click();
@@ -28,12 +22,31 @@ const App = () => {
 		const reader = new FileReader();
 
         reader.onload = function (e) {
+			// file size fileInput.files[0].size
 			photoPreview.current.src = e.target.result;
-			setPhoto({ src: e.target.result });
         }
 
         reader.readAsDataURL(fileInput.files[0]);
 	}
+
+	// this code will load the image in the background to get meta eg. size, width, height
+	// then the promise is resolved/returned for saving process of photo, skipping for now for mvp test
+	// this is for getting image dimensions
+	// const getImageMeta = new Promise(fileInput) => {
+	// 	const reader = new FileReader();
+
+    //     reader.onload = function (e) {
+	// 		// file size fileInput.files[0].size
+	// 		photoPreview.current.src = e.target.result;
+	// 		setPhoto({ src: e.target.result });
+    //     }
+
+    //     reader.readAsDataURL(fileInput.files[0])
+	// }
+
+	
+
+
 
 	const cameraCallback = (fileInput) => {
 		if (fileInput.files.length) {
@@ -43,13 +56,122 @@ const App = () => {
 		}
 	}
 
+	// pulls image from local storage dexie
+	const loadImage = () => {
+
+	}
+
+	// const getImageMeta = (file) => {
+	// 	const reader = new FileReader();
+
+    //     reader.onload = function (e) {
+	// 		console.log(e);
+	// 		return {
+	// 			width: e.
+	// 		}
+    //     }
+
+	// 	reader.readAsDataURL(file);
+	// };
+
+	const saveImage = async () => {
+		const file = fileInput.current.files[0];
+		const fileName = file.name.split(/(\\|\/)/g).pop();
+
+		offlineStorage.transaction('rw', offlineStorage.images, async() => {
+			if (await offlineStorage.images.add({
+				name: fileName,
+				fileSize: file.size, // KB
+				width: loadedPhoto.meta.width,
+				height: loadedPhoto.meta.height,
+				src: loadedPhoto.src
+			})) {
+				alert('Image saved');
+				setSavedPhotos(savedPhotos.push(fileName));
+			}
+		})
+		.catch(e => {
+			alert('Failed to save image to device');
+			console.log(e);
+		});
+	}
+
+	const setupOfflineStorage = () => { // width height come from preview
+		console.log('create db');
+		const db = new Dexie("LocalImageDatabase");
+		db.version(1).stores({ images: "++id,name,fileSize,width,height" }); // can add additional meta
+		db.open().then(function (db) {
+			// Database opened successfully
+			db.images.toArray().then((images) => {
+				setSavedPhotos(images);
+			});
+		}).catch (function (err) {
+			// handle this failure correctly
+			alert('failed to open local storage');
+		});
+		setOfflineStorage(db);
+	};
+
+	// check local storage eg. dexie on first run
+	useEffect(() => {
+		setupOfflineStorage();
+	}, []);
+
+	// const getOfflineStoragePhotos = async () => {
+	// 	const deviceStoredImages = await offlineStorage.images.toArray();
+	// 	return deviceStoredImages;
+	// };
+
+	// // when db set
+	useEffect(() => {
+	// 	console.log('os', offlineStorage);
+	// 	console.log(getOfflineStoragePhotos());
+	// 	// savedPhotos
+	
+	// pull an image from storage
+
+	console.log(savedPhotos.length);
+
+	}, [offlineStorage]); // listen to this change
+
+	const setLoadedImageMeta = (loadedImage) => {
+		setLoadedPhoto({ 
+			src: loadedImage.src,
+			meta: {
+				width: loadedImage.width,
+				height: loadedImage.height
+			} = loadedImage }); // lol destructure works
+	};
+
+	const getImagePreviewAspectRatioClass = () => {
+		const imageMetaSet = Object.keys(loadedPhoto.meta).length;
+
+		if (imageMetaSet) {
+			const imageMeta = loadedPhoto.meta;
+			return !(imageMeta.width >= imageMeta.height) // flipped
+				? "App__image-preview landscape"
+				: "App__image-preview portrait";	
+		} else {
+			return "App__image-preview";
+		}
+	}
+
+	const getSourceFromStorage = () => {
+		console.log(savedPhotos);
+		return savedPhotos.length ? savedPhotos[1].src : "";
+	}
+
 	return (
 		<div className="App">
-			<div className={ photo.src ? "App__image-preview-wrapper" : "App__image-preview-wrapper hidden"}>
-				<img className="App__image-preview" ref={ photoPreview } />
+			<div className={ loadedPhoto.src ? "App__image-preview-wrapper" : "App__image-preview-wrapper hidden"}>
+				<img className={ getImagePreviewAspectRatioClass() } ref={ photoPreview } onLoad={ () => setLoadedImageMeta(photoPreview.current) } />
 			</div>
 			<input type="file" ref={ fileInput } onChange={ () => { cameraCallback(fileInput.current) } } id="test-camera" />
 			<button type="button" onClick={ openCamera }>Take Picture</button>
+			<button type="button" className={ loadedPhoto.src ? "App__image-save" : "App__image-save hidden" } onClick={ savedPhotos.length ? loadImage : saveImage }>{
+				savedPhotos.length ? "Load image" : "Save Image To Device"
+			}</button>
+			<img class={ savedPhotos.length ? "App__loaded-string-img" : "App__loaded-string-img hidden"} src={ getSourceFromStorage() } />
 		</div>
 	);
 }
