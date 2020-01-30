231c662024acae3e3034ef48df4aac28478da218
Jacob Cunningham <jcun4128@gmail.com>
2020-01-29 18:08:36 -0600

    functional camera capture with preview

--- front-end/TODO.md
+++ front-end/TODO.md
@@ -1,3 +1,25 @@
+### PWA Offline MVP
+This proves that PWA can work offline regarding storing photos temporarily without access to file system and using the offline/online event to then upload the photos that are being stored as strings in IndexedDB.
+- [ ] basic test interface
+    - [ ] loads camera
+    - [ ] takes picture
+    - [ ] turn to string
+    - [ ] using PouchDB(IndexedDB wrappr)
+        - [ ] store photo
+    - [ ] use the offline/online sync to make request with photo as payload to remote site
+    - [ ] test pwa(install)
+        - [ ] needs domain
+        - [ ] needs ssl
+        - [ ] fix icon requirements
+        - [ ] test in emulated iPhone on services like Browserstack
+            - [ ] test that can pin
+            - [ ] if possible test the whole offline/online thing
+            - [ ] camera probably useless but could preload it with info for testing
+
+### Additional tasks from MVP above
+- ability to take multiple pictures at once/present/keep track
+
+
 ### Pages to build
 - [ ] Login/Register
 - [ ] View addresses/search
--- front-end/tagging-tracker-pwa/src/App.js
+++ front-end/tagging-tracker-pwa/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [camera, setCamera] = useState({ enabled: false }); // not sure if needed
+	const [photo, setPhoto] = useState({ src: "" });
+
+	const fileInput = useRef(null);
+	const photoPreview = useRef(null);
+
+	const getMedia = async (constraints) => {
+		let stream = null;
+		try {
+		  stream = await navigator.mediaDevices.getUserMedia(constraints);
+		  /* use the stream */
+		} catch(err) {
+			alert('Failed to get camera');
+		}
+	  }
+
+	const openCamera = () => {
+		fileInput.current.click();
+	}
+
+	// https://stackoverflow.com/questions/18457340/how-to-preview-selected-image-in-input-type-file-in-popup-using-jquery
+	const previewPhoto = (fileInput) => {
+		const reader = new FileReader();
+
+        reader.onload = function (e) {
+			photoPreview.current.src = e.target.result;
+			setPhoto({ src: e.target.result });
+        }
+
+        reader.readAsDataURL(fileInput.files[0]);
+	}
+
+	const cameraCallback = (fileInput) => {
+		if (fileInput.files.length) {
+			previewPhoto(fileInput);
+		} else {
+
+		}
+	}
+
+	return (
+		<div className="App">
+			<div className={ photo.src ? "App__image-preview-wrapper" : "App__image-preview-wrapper hidden"}>
+				<img className="App__image-preview" ref={ photoPreview } />
+			</div>
+			<input type="file" ref={ fileInput } onChange={ () => { cameraCallback(fileInput.current) } } id="test-camera" />
+			<button type="button" onClick={ openCamera }>Take Picture</button>
+		</div>
+	);
 }
 
 export default App;
